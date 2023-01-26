b75d1395926017b416ff20116301b1d52cc61db3
wikomangoli <Wikomangalo462gmail.com>
2023-01-26 07:58:31 -0400

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>git remote</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sint accusantium maxime quis temporibus explicabo nam fugit laudantium quas earum modi, reiciendis culpa itaque commodi reprehenderit laborum vero quasi quae?</p>
+    
+</body>
+</html>
